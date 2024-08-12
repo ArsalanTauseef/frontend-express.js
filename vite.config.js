ff41839a15cf6d40ab611ba41bebc9d90ce9c5b4
ArsalanTauseef <142938248+ArsalanTauseef@users.noreply.github.com>
2024-08-12 07:43:49 +0500

    new changes

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'https://backend-express-js-two.vercel.app',
-    }
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': 'https://backend-express-js-two.vercel.app',
+  //   }
+  // },
   plugins: [react()],
 })
